3bba742a98c4944fd638d5c9e230de47dea9bf1f
rcpeters <info@rcpeters.com>
2015-06-03 15:49:02 -0700

    stripped down node

---
+++ README.md
@@ -0,0 +1,17 @@
+
+#### Install NodeJS or Upgrade 
+
+[Install it!](https://nodejs.org/)
+or 
+[Upgrade it!](http://davidwalsh.name/upgrade-nodejs)
+if the version if less then 0.12.0
+
+### Install dependencies
+
+       npm install 
+
+### Start server
+
+       node server.js
+
+### Visit [http://localhost:8000/](http://localhost:8000/)
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var port = process.env.EXPRESSPORT != undefined ? process.env.EXPRESSPORT: '8000'
+
+// Set the client credentials and the OAuth2 server
+var credentials = {
+  clientID: 'APP-O9TUKAPVLALU1SOJ',
+  clientSecret: '0eafb938-020e-45a6-a148-3c222171d9d8',
+  site: 'https://sandbox.orcid.org',
+  tokenPath:'https://api.sandbox.orcid.org/oauth/token'
+};
+
+// Initialize the OAuth2 Library
+var oauth2 = require('simple-oauth2')(credentials);
+
+// Authorization oauth2 URI
+var authorization_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: 'http://localhost:8000/callback',
+  scope: '/authenticate',
+  state: 'nope'
+});
+
+// init express
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+  
+// Index page 
+app.get('/', function(req, res) {
+  res.render('pages/index', {
+    'authorization_uri': authorization_uri
+  });
+});
+
+// Get the access token object (the authorization code is given from the previous step).
+app.get('/callback', function(req, res) {
+  var token;
+  var code = req.query.code;
+  oauth2.authCode.getToken({
+    code: code,
+    redirect_uri: 'http://localhost:8000/callback'
+  }, function(error, result){
+    if (error) {
+      // Show error
+      res.render('pages/error', {
+        'error': JSON.stringify(error, null, 4)
+      });
+    } else {
+      // Show token
+      token = oauth2.accessToken.create(result);
+      res.render('pages/token', {
+        'token': JSON.stringify(token, null, 4)
+      })
+    }
+  });
+});
+
+app.listen(port, function (){
+  console.log('server started on ' + port);
+});
