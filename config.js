d68f5788e39ba7b0ce68483cdcff8a99b71c4bd8
rcpeters <info@rcpeters.com>
2015-06-08 12:08:51 -0700

    add openshit support

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = config = {
   PORT: '8000',
+  SERVER_IP: '127.0.0.1',
   CLIENT_ID: 'APP-O9TUKAPVLALU1SOJ',
   CLIENT_SECRET: '0eafb938-020e-45a6-a148-3c222171d9d8',
   AUTH_SITE: 'https://sandbox.orcid.org',
@@ -11,3 +12,9 @@ module.exports = config = {
 for (key in config)
 	if (process.env[key] != undefined)
 		config[key] = process.env[key];
+
+// overrides for www.openshift.com deployment
+if (process.env.OPENSHIFT_NODEJS_IP != undefined) 
+	config.SERVER_IP = process.env.OPENSHIFT_NODEJS_IP;
+if (process.env.OPENSHIFT_NODEJS_PORT != undefined) 
+	config.PORT = process.env.OPENSHIFT_NODEJS_PORT;
